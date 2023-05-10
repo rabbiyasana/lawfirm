b7f10c5f3e203bb59206528e64c1edde2605b95a
Rabbia sana <rabbiasana55@gmail.com>
2023-05-11 01:55:37 +0500

    delete extra files

--- nav.js
+++ nav.js
@@ -1,28 +1,28 @@
 document.getElementById("navMenu").innerHTML =
-'<div class="container-fluid bg-dark">'+
-		'<div class="container">'+
-			'<div class="row ">'+
-            '<div class="col-xl-3 col-lg-3 col-md-3 col-xs-12 text-center mt-3" >'+
-                    '<img src="imgs/logo.png" class="text-warning">'+
-               '</div>'+
-			'<div class="col-xl-8 col-lg-8 col-md-8 col-xs-4 mt-3 ml-4">'+
-          '<div class="navbar navbar-expand-md bg-dark text-warning">'+
-        '<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">'+
-            '<span class="fa fa-navicon"></span>'+
-        '</button>'+
+`<div class="container-fluid bg-dark">
+		<div class="container">
+			<div class="row ">
+            <div class="col-xl-3 col-lg-3 col-md-3 col-xs-12 text-center mt-3" >
+                    <img src="imgs/logo.png" class="text-warning">
+               </div>
+			<div class="col-xl-8 col-lg-8 col-md-8 col-xs-4 mt-3 ml-4">
+          <div class="navbar navbar-expand-md bg-dark text-warning">
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="fa fa-navicon"></span>
+        </button>
 
-        '<div class="collapse navbar-collapse" id="navbarCollapse">'+
-            '<div class="navbar-nav">'+
-                '<a href="index.html" class="nav-item nav-link active text-warning mr-5">Home</a>'+
-                '<a href="aboutme.html" class="nav-item nav-link text-warning mr-5">About Me</a>'+
-                '<a href="testimonial.html" class="nav-item nav-link text-warning mr-5">Testimonial</a>'+
-                 '<a href="casestudy.html" class="nav-item nav-link text-warning  mr-5">Case Study</a>'+
-                '<a href="contact.html" class="nav-item nav-link text-warning mr-5">Contact Us</a>'+
-            '</div>'+
-        '</div>'+
-    '</div>'+
-		'</div>'+
-	       '</div>'+
-		'</div>'+
-	'</div>'
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="index.html" class="nav-item nav-link active text-warning mr-5">Home</a>
+                <a href="aboutme.html" class="nav-item nav-link text-warning mr-5">About Me</a>
+                <a href="testimonial.html" class="nav-item nav-link text-warning mr-5">Testimonial</a>
+                 <a href="casestudy.html" class="nav-item nav-link text-warning  mr-5">Case Study</a>
+                <a href="contact.html" class="nav-item nav-link text-warning mr-5">Contact Us</a>
+            </div>
+        </div>
+    </div>
+		</div>
+	       </div>
+		</div>
+	</div>`
 ;
